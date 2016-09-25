28e48a1338ea2d70bc2be644d92646c847e443ea
Alexander Velichko <alvi@mail.ua>
2016-09-25 20:27:14 +0300

    all work without save state

--- js/main.js
+++ js/main.js
@@ -1,13 +1,32 @@
 "use strict";
-var timeSecond = 10;
+var timeSecond = 0;
+var timeRest = 0;
+var countPomodoro = 0;
 var stopFlag = false;
+var restFlag = false;
 var interval;
+var workTime = document.querySelector('.js-workTime');
+var restTime = document.querySelector('.js-restTime');
 var min = document.querySelector('.clock-panel__min');
 var sec = document.querySelector('.clock-panel__sec');
+var btnSettings = document.querySelector('.js-settings');
 var btnStart = document.querySelector('.control__btn-start ');
 var btnReset = document.querySelector('.control__btn-reset');
+var goalWrapper  = document.querySelector('.goals');
+var goalCurrent = document.querySelector('.goals__current');
+var settingsPanel = document.querySelector('.setting-panel');
+var wrapper = document.querySelector('.wrapper');
+refreshTimer();
+updateGoal();
 changeTimer();
+
+
+btnSettings.addEventListener('click', function () {
+  settingsPanel.classList.toggle('open');
+});
+
 btnStart.addEventListener('click', function () {
+  btnStart.classList.toggle('control__btn-pause');
   if (!stopFlag) {
     startTimer();
     stopFlag = true;
@@ -17,8 +36,9 @@ btnStart.addEventListener('click', function () {
 });
 
 btnReset.addEventListener('click', function () {
+  btnStart.classList.remove('control__btn-pause');
   stopTimer(interval);
-  timeSecond = 10;
+  refreshTimer();
   changeTimer();
 });
 
@@ -27,6 +47,18 @@ var stopTimer = function (timerName) {
   clearInterval(timerName);
 };
 
+function updateGoal() {
+  if(countPomodoro >= 12){
+    goalWrapper.innerHTML = 'Отлично поработали сегодня!';
+  }else {
+    goalCurrent.innerHTML = countPomodoro;
+  }
+}
+
+function  refreshTimer() {
+  timeSecond = workTime.value*60;
+  timeRest = restTime.value * 60;
+}
 function changeTimer() {
   if (Math.floor(timeSecond / 60) < 10) {
     min.innerHTML = '0' + Math.floor(timeSecond / 60);
@@ -39,9 +71,19 @@ function changeTimer() {
     sec.innerHTML = Math.floor(timeSecond % 60);
   }
   if (timeSecond === 0) {
+    if(!restFlag){
+      countPomodoro++;
+      updateGoal();
+      timeSecond = timeRest;
+      wrapper.classList.add('wrapper--blue');
+      restFlag = true;
+    } else {
+      wrapper.classList.remove('wrapper--blue');
+      refreshTimer();
+    }
     stopTimer(interval);
-    timeSecond = 10;
-    alert('пора отдохнуть');
+    startTimer();
+    stopFlag = true;
   }
 }
   
@@ -49,9 +91,6 @@ var startTimer = function () {
   window.interval = window.setInterval(function () {
     timeSecond--;
     changeTimer();
-    console.log(timeSecond);
   }, 1000);
-  
 };
-
 
